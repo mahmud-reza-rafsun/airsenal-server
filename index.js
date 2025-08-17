e98870d4f0fe57ae0fcc0dcdf2f71aa735370400
Rafsun <rafsun@rafsun.dev>
2025-08-17 23:30:18 +0600

    resolve error

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
+        // await client.connect();
         // all collcetions
         const userCollcetions = client.db('AIrsenal').collection('users');
         const productCollcetions = client.db('AIrsenal').collection('products');
@@ -320,7 +320,7 @@ async function run() {
         })
 
         // get all coupon in db
-        app.get('/get-coupon', verifyToken, async (req, res) => {
+        app.get('/get-coupon', async (req, res) => {
             const result = await couponCollcetions.find().toArray();
             res.send(result);
         })
@@ -352,7 +352,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
